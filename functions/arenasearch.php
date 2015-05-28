cb0c34200cf6c0202d8203d8797f7c84b9dd5c1d
125125 <tim-levinsson@hotmail.com>
2015-05-28 18:46:42 +0200

    Update arenasearch.php

--- functions/arenasearch.php
+++ functions/arenasearch.php
@@ -3,7 +3,6 @@ include("../config/db_conf.php");
 mysql_connect($host, $dbuser, $dbpass) or die(mysql_error());
 mysql_select_db($dbname) or die(mysql_error());
  
-// Get all the data from the "example" table
 $escaped_string = mysql_real_escape_string($_GET['name']);
 If ($escaped_string == '') { exit; }
 $type = $_GET['type'];
