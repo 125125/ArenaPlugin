c355f780c9a007ecb52d8d70623de978f7c99ca1
125125 <tim-levinsson@hotmail.com>
2015-05-28 18:46:12 +0200

    Update body.php

--- include/body.php
+++ include/body.php
@@ -16,7 +16,6 @@ $sql = "SELECT * FROM arena_team";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         $id = $row['arenaTeamId'];
 		switch($page) {
@@ -26,7 +25,6 @@ if (mysqli_num_rows($result) > 0) {
 		}
     }
 } else {
-    echo "0 results";
 }
 
 mysqli_close($conn);
